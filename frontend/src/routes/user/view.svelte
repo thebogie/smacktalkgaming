626501799c440b4aba5ff1a16dc35057af701a13
thebogie@live.com <thebogie@live.com>
2020-08-19 18:03:58 -0500

    trying preload for data grab in user page

--- frontend/src/routes/user/view.svelte
+++ frontend/src/routes/user/view.svelte
@@ -1,7 +1,28 @@
 <script context="module">
-  export function preload (page, session) {
+  import * as api from "api";
+
+  let error = null;
+
+  export async function preload(page, session) {
     if (!session.user) {
-      return this.redirect(302, 'login')
+      return this.redirect(302, "login");
+    }
+
+    console.log("VIEW SESSION:" + JSON.stringify(session.user));
+
+    try {
+      //let user = await api.users.getUser({ userId: params.id });
+
+      const response = await api.users.getUser({ userId: session.user.userid });
+
+      console.log("FUCKINGUSER:" + JSON.stringify(response));
+      //userstats = await api.users.getUserStats({
+      // userId: $session.user.userid,
+      //daterange: "01011970"
+      //});
+    } catch (e) {
+      console.log("ERRROR: %s" + e);
+      error = e;
     }
   }
 </script>
@@ -9,3 +30,4 @@
 <svelte:head>
   <title>View Profile</title>
 </svelte:head>
+here
